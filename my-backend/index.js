3e489916c741390e13eafe79509ac39eb0949bac
Karthini Balakrishnan <karthinib@gmail.com>
2025-06-30 13:28:07 +0530

    db_url added

--- my-backend/index.js
+++ my-backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const dbUrl = process.env.MONGODB_URI;
 
 app.get('/', (req, res) => {
   res.send('Hello from Render backend!');
